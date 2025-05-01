bec4b93d0390aa839f5a2bebfbbf7d18b6180207
hemingyang_ClionGit <ttai33638@gamil.com>
2025-05-01 14:56:34 +0800

    commit

---
+++ src/test/java/com/example/demo/OssJavaSdkQuickStart.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import java.io.*;
+import java.util.Random;
+
+import com.aliyun.oss.*;
+import com.aliyun.oss.model.OSSObject;
+import com.aliyun.oss.model.ObjectListing;
+import com.aliyun.oss.model.OSSObjectSummary;
+import com.aliyun.oss.common.auth.*;
+import com.aliyun.oss.common.comm.SignVersion;
+
+public class OssJavaSdkQuickStart {
+
+    public static void main(String[] args)  {
+
+    }
+}
